688127467010018161bdedfe84d61e327b43cfa9
Kihyeon Kwon <kihyeon.kwn@gmail.com>
2022-08-31 12:00:00 +0900

    JKH-108 FEAT:scaffold와 bottomNavBar 기능 추가

--- lib/screens/main_screens.dart
+++ lib/screens/main_screens.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MainScreens extends StatefulWidget {
@@ -8,11 +9,41 @@ class MainScreens extends StatefulWidget {
 }
 
 class _MainScreensState extends State<MainScreens> {
+  int _selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: const Text('MainScreens'),
+    return Scaffold(
+      body: IndexedStack(index: _selectedIndex, children: [
+        Container(
+            color: Colors.orange,
+            child: const Center(
+                child: Text('Indexed Stack 1',
+                    style: TextStyle(fontSize: 20, color: Colors.black)))),
+        Container(
+            color: Colors.redAccent[100],
+            child: const Center(
+                child: Text('Indexed Stack 2',
+                    style: TextStyle(fontSize: 20, color: Colors.black)))),
+        Container(
+            color: Colors.orange,
+            child: const Center(
+                child: Text('Indexed Stack 3',
+                    style: TextStyle(fontSize: 20, color: Colors.black))))
+      ]),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(CupertinoIcons.home), label: '홈'),
+          BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.chat_bubble), label: '채팅'),
+          BottomNavigationBarItem(icon: Icon(CupertinoIcons.home), label: '홈')
+        ],
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        currentIndex: _selectedIndex,
       ),
     );
   }
