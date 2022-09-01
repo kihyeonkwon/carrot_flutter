d5e2d5404d475f8b92da5db1c0ddfc035c4b986e
Kihyeon Kwon <kihyeon.kwn@gmail.com>
2022-09-01 23:48:31 +0900

    JKH-111 나의화면만들기 #comment 기본 화면 구조 생성

--- lib/screens/my_carrot/my_carrot_screen.dart
+++ lib/screens/my_carrot/my_carrot_screen.dart
@@ -5,8 +5,28 @@ class MyCarrotScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('my screen'),
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      appBar: AppBar(
+        title: const Text('나의 당근'),
+        actions: [
+          IconButton(icon: const Icon(Icons.settings), onPressed: () {}),
+        ],
+        bottom: const PreferredSize(
+            preferredSize: Size.fromHeight(0.5),
+            child: Divider(thickness: 0.5, height: 0.5, color: Colors.grey)),
+      ),
+      body: ListView(
+        children: [
+          //MyCarrotHeader(),
+          const SizedBox(height: 8),
+          //CardIconMenu(iconMenuList:iconMenu1),
+          const SizedBox(height: 8.0),
+          // CardIconMenu(iconMenuList:iconMenu2),
+          const SizedBox(height: 8),
+          // CardIconMenu(iconMenuList:iconMenu3),
+        ],
+      ),
     );
   }
 }
