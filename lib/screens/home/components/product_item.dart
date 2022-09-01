12469fcb4596d5d2103ff867f090b979d551a7a3
Kihyeon Kwon <kihyeon.kwn@gmail.com>
2022-09-01 22:26:33 +0900

    JKH-110 #comment FEAT: 홈리스트에 사용할 컴포넌트인 product item 위젯 만들기

---
+++ lib/screens/home/components/product_item.dart
@@ -0,0 +1,27 @@
+import 'package:carrot/models/product.dart';
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatelessWidget {
+  final Product product;
+
+  const ProductItem({required this.product, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 135.0,
+      padding: const EdgeInsets.all(16),
+      child: Row(children: [
+        ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Image.network(
+              product.urlToImage,
+              width: 15,
+              height: 115,
+              fit: BoxFit.cover,
+            )),
+        const SizedBox(width: 16)
+      ]),
+    );
+  }
+}
