52d904083a232c76e948ae24c6ce712209818487
Kihyeon Kwon <kihyeon.kwn@gmail.com>
2022-09-03 00:07:47 +0900

    JKH-111 나의화면만들기 #comment 설정 메뉴 리스트 생성

---
+++ lib/screens/my_carrot/components/card_icon_menu.dart
@@ -0,0 +1,37 @@
+import 'package:carrot/models/icon_menu.dart';
+import 'package:carrot/theme.dart';
+import 'package:flutter/material.dart';
+
+class CardIconMenu extends StatelessWidget {
+  final List<IconMenu> iconMenuList;
+
+  const CardIconMenu({required this.iconMenuList, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        elevation: 0.5,
+        margin: EdgeInsets.zero,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(0)),
+        child: Padding(
+            padding: const EdgeInsets.all(15),
+            child: Column(
+              children: List.generate(
+                  iconMenuList.length,
+                  (index) => _buildRowIconItem(
+                      iconMenuList[index].title, iconMenuList[index].iconData)),
+            )));
+  }
+}
+
+Widget _buildRowIconItem(String title, IconData iconData) {
+  return Container(
+      height: 50,
+      child: Row(children: [
+        Icon(iconData, size: 17),
+        const SizedBox(
+          width: 20,
+        ),
+        Text(title, style: textTheme().subtitle1)
+      ]));
+}
